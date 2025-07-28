73a0b601910f30690d3b2a57d4ec88237136a103
Juleslgc <venturajules99@gmail.com>
2025-07-28 14:09:09 +0200

    feat: add function that return sum of a mix list using type annotations

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    sum: float = 0.0
+    for i in mxd_lst:
+        sum += i
+    return sum
