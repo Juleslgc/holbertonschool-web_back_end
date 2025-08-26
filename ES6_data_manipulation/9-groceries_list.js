67f71cbc61c4d6d74afae4bf846d24cbbb2f4612
Juleslgc <venturajules99@gmail.com>
2025-08-26 10:40:40 +0200

    feat: added new file 9-groceries_list

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+  const dict = new Map();
+
+  dict.set('Apples', 10)
+    .set('Tomatoes', 10)
+    .set('Pasta', 1)
+    .set('Rice', 1)
+    .set('Banana', 5);
+
+  return dict;
+}
