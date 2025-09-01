73d7f17ac2b4590b0d6e99909cbf1b3449d3fb6a
Juleslgc <venturajules99@gmail.com>
2025-09-01 12:40:19 +0200

    feat: add new file that using database

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+function countStudents(database) {
+  if (!fs.existsSync(database)) {
+    throw new Error('Cannot load the database');
+  } else {
+    const data = fs.readFileSync(database, 'utf8');
+    const lines = data.trim().split('\n'); // Séparer les lignes
+    const headers = lines[0].split(','); // Première ligne = noms des colonnes
+    const students = [];
+    for (let i = 1; i < lines.length; i += 1) {
+      const values = lines[i].split(',');
+      const obj = {};
+      headers.forEach((header, index) => {
+        obj[header] = values[index];
+      });
+      students.push(obj);
+    }
+    process.stdout.write(`Number of students: ${students.length}\n`);
+
+    const csStudents = students.filter((student) => student.field === 'CS');
+    const csName = csStudents.map((student) => student.firstname);
+    process.stdout.write(`Number of students in CS: ${csStudents.length}. List: ${csName.join(', ')}\n`);
+
+    const sweStudents = students.filter((student) => student.field === 'SWE');
+    const sweName = sweStudents.map((student) => student.firstname);
+    process.stdout.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweName.join(', ')}\n`);
+  }
+}
+module.exports = countStudents;
