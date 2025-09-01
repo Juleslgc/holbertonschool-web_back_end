3bbba5c3fe22981cdab17aeb57c2359c551ec27d
Juleslgc <venturajules99@gmail.com>
2025-09-01 14:44:42 +0200

    feat: added new file that read database file asynchronously

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+async function countStudents(database) {
+  try {
+    const data = await fs.readFileSync(database, 'utf8');
+    const lines = data.trim().split('\n');
+    const headers = lines[0].split(',');
+    const students = [];
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const value = lines[i].split(',');
+      const obj = {};
+      headers.forEach((header, index) => {
+        obj[header] = value[index];
+      });
+      students.push(obj);
+    }
+    process.stdout.write(`Number of students: ${students.length}\n`);
+
+    const csStudents = students.filter((student) => student.field === 'CS');
+    const csName = csStudents.map((student) => student.firstname);
+    const csfield = csStudents.length > 0 ? csStudents[0].field : 'CS';
+    process.stdout.write(`Number of students in ${csfield}: ${csStudents.length}. List: ${csName.join(', ')}\n`);
+
+    const sweStudents = students.filter((student) => student.field === 'SWE');
+    const sweName = sweStudents.map((student) => student.firstname);
+    const swefield = sweStudents.length > 0 ? sweStudents[0].field : 'SWE';
+    process.stdout.write(`Number of students in ${swefield}: ${sweStudents.length}. List: ${sweName.join(', ')}\n`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
