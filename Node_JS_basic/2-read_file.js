e7ea42ed552aeeb165ea2aeda640224155206b86
Juleslgc <venturajules99@gmail.com>
2025-09-02 10:29:47 +0200

    Fix task 2: Replace process.stdout.write by console.log

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -16,17 +16,17 @@ function countStudents(database) {
       });
       students.push(obj);
     }
-    process.stdout.write(`Number of students: ${students.length}\n`);
+    console.log(`Number of students: ${students.length}\n`);
 
     const csStudents = students.filter((student) => student.field === 'CS');
     const csName = csStudents.map((student) => student.firstname);
     const csfield = csStudents.length > 0 ? csStudents[0].field : 'CS';
-    process.stdout.write(`Number of students in ${csfield}: ${csStudents.length}. List: ${csName.join(', ')}\n`);
+    console.log(`Number of students in ${csfield}: ${csStudents.length}. List: ${csName.join(', ')}\n`);
 
     const sweStudents = students.filter((student) => student.field === 'SWE');
     const sweName = sweStudents.map((student) => student.firstname);
     const swefield = sweStudents.length > 0 ? sweStudents[0].field : 'SWE';
-    process.stdout.write(`Number of students in ${swefield}: ${sweStudents.length}. List: ${sweName.join(', ')}\n`);
+    console.log(`Number of students in ${swefield}: ${sweStudents.length}. List: ${sweName.join(', ')}\n`);
   }
 }
 module.exports = countStudents;
